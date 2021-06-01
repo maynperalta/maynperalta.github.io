2498295e5b060db7a329834dfd5309d88f5fdd5c
Maynard Peralta <maynperalta@gmail.com>
2021-05-31 22:48:17 -0600

    created navbar and placeholder pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+import About from "./About";
+import { Route, Link } from "react-router-dom";
+import Navbar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
     </div>
   );
 }
---
+++ src/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Portfolio(){
+    return (
+        <div>
+            <h1>Under Construction!</h1>
+        </div>
+    );
+}
+
+export default Portfolio;
